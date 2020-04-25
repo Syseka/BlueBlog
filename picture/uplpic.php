9d3caa18d46afa51bb79fc90edc86cd994db1239
Syseka <62742295+Syseka@users.noreply.github.com>
2020-04-25 19:48:04 +0300

    Update uplpic.php
    Добавил комментарии.

--- picture/uplpic.php
+++ picture/uplpic.php
@@ -6,17 +6,20 @@
 if ($_FILES)
     {
     $f_name = $_FILES['pict']['name'];
+// получаю файл из хтмл-формы ввода
     switch($_FILES['pict']['type'])
         {
         case 'image/jpeg': $ext = 'jpg'; break;
         case 'image/gif': $ext = 'gif'; break;
         case 'image/png': $ext = 'png'; break;
         default: $ext = ''; break;
+// проверка форматов
         }
     if ($ext)
         {
             $picn = "$f_name";
             move_uploaded_file($_FILES['pict']['tmp_name'], 'picture/'.$picn);
+// перемещает файлы в нужную мне папку
         }
 	else echo "'$f_name' — ничего не получено или ты необучаемый.<br>";
     }
